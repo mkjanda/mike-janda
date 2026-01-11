cb3c92c5c7a17fc26d6301d9ef54ef7cade7dee0
Michael Janda <admin@iatsoftware.net>
2026-01-11 18:19:42 -0500

    v1.1

--- scripts/modules/writing-page.js
+++ scripts/modules/writing-page.js
@@ -4,8 +4,22 @@ define(['knockout', 'text!templates/writing-page.html', 'text!writing/writing.js
             let self = this;
             self.writing = JSON.parse(writingJSON);
             self.story = ko.observable(self.writing.stories[0]);
+            self.pageNum = ko.observable(1);
+            self.techSource = ko.pureComputed(() => `/images/book${self.pageNum()}.jpg`);
             self.displayedSection = ko.observable('Writing');          
             self.onStoryClick = self._onStoryClick.bind(self);
+            self.bookClick = self._bookClick.bind(self);
+        }
+
+        _bookClick(evt) {
+            const self = this;
+            let book = document.querySelector('img.iat-manual');
+            let change = (book.clientWidth >> 1) > evt.clientX ? -1 : 1;
+            if ((change === -1) && (self.pageNum() === 1))
+                return;
+            if ((change === 1) && (self.pageNum() === 5))
+                return;
+            self.pageNum(self.pageNum() + change);
         }
 
         _onStoryClick(ndx) {
